5f728c410099cf6228422b467ceaa0abc6b76519
Bruno <brunosuportefs@gmail.com>
2019-09-03 11:11:37 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/bruno/ecommerce/EcommerceApplication.java
+++ src/main/java/br/com/bruno/ecommerce/EcommerceApplication.java
@@ -1,13 +1,31 @@
 package br.com.bruno.ecommerce;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.bruno.ecommerce.domain.Categoria;
+import br.com.bruno.ecommerce.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
