fb0cfff8190751c6e5787220661dc1b850c04176
Bruno <brunosuportefs@gmail.com>
2019-09-03 14:01:41 -0300

    Endpoint clientes/id disponivel

---
+++ src/main/java/br/com/bruno/ecommerce/resource/ClienteResource.java
@@ -0,0 +1,25 @@
+package br.com.bruno.ecommerce.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.bruno.ecommerce.domain.Cliente;
+import br.com.bruno.ecommerce.service.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable("id") Integer id){
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
