4040ab472d0920f7dc0f8d260148f26f235463b0
Bruno <brunosuportefs@gmail.com>
2019-09-03 08:27:11 -0300

    Criando Repository para CategoriaService

--- src/main/java/br/com/bruno/ecommerce/resource/CategoriaResource.java
+++ src/main/java/br/com/bruno/ecommerce/resource/CategoriaResource.java
@@ -1,27 +1,24 @@
 package br.com.bruno.ecommerce.resource;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bruno.ecommerce.domain.Categoria;
+import br.com.bruno.ecommerce.service.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
 
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-
-		List<Categoria> categorias = new ArrayList<>();
-		categorias.add(cat1);
-		categorias.add(cat2);
-
-		return categorias;
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
