ac72f8ba1f276a5f09ec5f0c4514ba3231a5b437
Bruno <brunosuportefs@gmail.com>
2019-09-06 13:23:14 -0300

    Listando todas as Categorias com DTO

--- src/main/java/br/com/bruno/ecommerce/resource/CategoriaResource.java
+++ src/main/java/br/com/bruno/ecommerce/resource/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.bruno.ecommerce.resource;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.bruno.ecommerce.domain.Categoria;
+import br.com.bruno.ecommerce.dto.CategoriaDTO;
 import br.com.bruno.ecommerce.service.CategoriaService;
 
 @RestController
@@ -45,5 +48,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> lista = service.findAll();
+		List<CategoriaDTO> listDTO = lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/br/com/bruno/ecommerce/service/CategoriaService.java
+++ src/main/java/br/com/bruno/ecommerce/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.bruno.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,5 +40,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityExcpetion("Não é possível excluir uma categoria que possui produtos!");
 		}		
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
