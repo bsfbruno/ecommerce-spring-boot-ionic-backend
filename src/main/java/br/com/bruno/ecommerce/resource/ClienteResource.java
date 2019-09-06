3f865bcef90a6964bb717b78df8a8af59ef276f6
Bruno <brunosuportefs@gmail.com>
2019-09-06 16:19:21 -0300

    Inserindo um novo cliente com Post

--- src/main/java/br/com/bruno/ecommerce/resource/ClienteResource.java
+++ src/main/java/br/com/bruno/ecommerce/resource/ClienteResource.java
@@ -1,5 +1,6 @@
 package br.com.bruno.ecommerce.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.bruno.ecommerce.domain.Cliente;
 import br.com.bruno.ecommerce.dto.ClienteDTO;
+import br.com.bruno.ecommerce.dto.ClienteNewDTO;
 import br.com.bruno.ecommerce.service.ClienteService;
 
 @RestController
@@ -30,6 +33,15 @@ public class ClienteResource {
 	public ResponseEntity<?> find(@PathVariable("id") Integer id){
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@Valid /*faz o json ser conv. para java*/@RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				  .path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/br/com/bruno/ecommerce/service/ClienteService.java
+++ src/main/java/br/com/bruno/ecommerce/service/ClienteService.java
@@ -10,9 +10,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.bruno.ecommerce.domain.Cidade;
 import br.com.bruno.ecommerce.domain.Cliente;
+import br.com.bruno.ecommerce.domain.Endereco;
+import br.com.bruno.ecommerce.domain.enums.TipoCliente;
 import br.com.bruno.ecommerce.dto.ClienteDTO;
+import br.com.bruno.ecommerce.dto.ClienteNewDTO;
 import br.com.bruno.ecommerce.repository.ClienteRepository;
+import br.com.bruno.ecommerce.repository.EnderecoRepository;
 import br.com.bruno.ecommerce.service.exception.DataIntegrityExcpetion;
 import br.com.bruno.ecommerce.service.exception.ObjectNotFoundException;
 
@@ -21,11 +26,21 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id +
 				", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -50,6 +65,22 @@ public class ClienteService {
 	public Page<Cliente> findPage(Integer page /*número da página que vai ser carregada*/, Integer linesPerPage /*número de linhas que serão retornados*/, String orderBy /*tipo de ordenação*/, String direction /*ascendente ou descendente*/) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO obj) {
+		Cliente cli = new Cliente(null, obj.getNome(), obj.getEmail(), obj.getCpfOuCnpj(), TipoCliente.toEnum(obj.getTipo()));
+		Cidade cid = new Cidade(obj.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, obj.getLogradouro(), obj.getNumero(), obj.getComplemento(), obj.getBairro(), obj.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(obj.getTelefone1());
+		if (obj.getTelefone2() != null) {
+			cli.getTelefones().add(obj.getTelefone2());
+		}
+
+		if (obj.getTelefone3() != null) {
+			cli.getTelefones().add(obj.getTelefone3());
+		}
+		return cli;
 	}
 
 	public Cliente fromDTO(ClienteDTO obj) {
