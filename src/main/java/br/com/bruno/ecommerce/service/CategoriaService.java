0562a17047d6b4d9cd87957d243ae395f48a17dd
Bruno <brunosuportefs@gmail.com>
2019-09-06 13:26:30 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/br/com/bruno/ecommerce/service/CategoriaService.java
+++ src/main/java/br/com/bruno/ecommerce/service/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.bruno.ecommerce.domain.Categoria;
@@ -44,5 +47,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page /*número da página que vai ser carregada*/, Integer linesPerPage /*número de linhas que serão retornados*/, String orderBy /*tipo de ordenação*/, String direction /*ascendente ou descendente*/) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
