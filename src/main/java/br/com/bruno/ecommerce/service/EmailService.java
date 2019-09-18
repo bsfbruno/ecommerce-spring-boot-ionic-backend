ba34eef9a2c6fabb4da0366c08653f10d7e39972
Bruno <bruno44sf@gmail.com>
2019-09-18 11:57:52 -0300

    Esqueci a senha

--- src/main/java/br/com/bruno/ecommerce/service/EmailService.java
+++ src/main/java/br/com/bruno/ecommerce/service/EmailService.java
@@ -2,6 +2,7 @@ package br.com.bruno.ecommerce.service;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import br.com.bruno.ecommerce.domain.Cliente;
 import br.com.bruno.ecommerce.domain.Pedido;
 
 public interface EmailService {
@@ -9,4 +10,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido Obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
