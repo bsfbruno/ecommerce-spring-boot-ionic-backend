8fbf90acdc5b8edaf6609d978ac30a4ce02091af
Bruno <bruno44sf@gmail.com>
2019-09-19 10:07:29 -0300

    Enviando imagem via endpoint

--- src/main/java/br/com/bruno/ecommerce/EcommerceApplication.java
+++ src/main/java/br/com/bruno/ecommerce/EcommerceApplication.java
@@ -1,17 +1,11 @@
 package br.com.bruno.ecommerce;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.bruno.ecommerce.service.S3Service;
-
 @SpringBootApplication
 public class EcommerceApplication implements CommandLineRunner{
-		
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
@@ -19,6 +13,6 @@ public class EcommerceApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\temp\\fotos\\ana.jpg");
+		
 	}
 }
