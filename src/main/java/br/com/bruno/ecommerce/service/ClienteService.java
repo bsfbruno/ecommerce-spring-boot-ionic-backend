09f11c673899335535d3ad3d21281f4f2bd165b3
Bruno <bruno44sf@gmail.com>
2019-09-21 10:29:00 -0300

    Ajustando tamanho da imagem

--- src/main/java/br/com/bruno/ecommerce/service/ClienteService.java
+++ src/main/java/br/com/bruno/ecommerce/service/ClienteService.java
@@ -46,6 +46,9 @@ public class ClienteService {
 	
 	@Autowired
 	private ImageService imageService;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
@@ -124,6 +127,9 @@ public class ClienteService {
 		}
 
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
